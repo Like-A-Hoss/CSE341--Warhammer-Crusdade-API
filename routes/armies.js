4fbd785c1f3fe0de873e954cf15eab41a376324a
Like-A-Hoss <hapleswithdice@gmail.com>
2023-05-20 22:02:19 -0500

    Army Route updated

--- routes/armies.js
+++ routes/armies.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const path = require('../controlers/armies');
 
 router.get('/', path.getAll);
+router.get('/:id', path.getSearch);
+router.post('/', path.postNew);
+
 
 module.exports = router;
