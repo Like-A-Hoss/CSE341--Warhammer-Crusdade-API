9fef2d613b6f9c8040f51b715843d90a89281c34
Like-A-Hoss <hapleswithdice@gmail.com>
2023-06-03 23:55:16 -0500

    Added updated validation

--- controlers/armies.js
+++ controlers/armies.js
@@ -75,24 +75,46 @@ const postNew = async (req,res) =>
 //update Resource Points
 const updateRp = async(req,res) =>
 {
-  const query = new objectId(req.params.id);
-  const updateField = req.body.updateField;
- 
+  try {
+    const { id } = req.params;
+    const { update } = req.body;
 
-  const result = await db_client.getDb().db(dbName).collection(colName).updateOne(
-    {_id:query},
-    { $set:{RP: updateField}},
-    {upsert:true}
-    );
-    if (result.modifiedCount > 0) {
-      res.status(204).send();
-    } else {
-      res.status(500).json(result.error || 'Some error occurred while updating the contact.');
+    // Check if the 'update' field exists and is an integer
+    if (typeof update === 'undefined' || !Number.isInteger(update)) {
+      return res.status(400).json({ error: 'Invalid input: Update field must be set and contain an integer.' });
     }
+    const query = new objectId(req.params.id);
+    const updateField = req.body.updateField;
+   
+  
+    const result = await db_client.getDb().db(dbName).collection(colName).updateOne(
+      {_id:query},
+      { $set:{RP: updateField}},
+      {upsert:true}
+      );
+      if (result.modifiedCount > 0) {
+        res.status(204).send();
+      } else {
+        res.status(500).json(result.error || 'Some error occurred while updating the contact.');
+      }
+  } catch (error) {
+    console.error('Error updating army:', error);
+    res.status(500).send('An Error occurred while updating your army.  Please check your inputs and try again');
+  }
+ 
 };
 
 const updateSize = async(req,res) =>
 {
+  try{
+    const { id } = req.params;
+    const { update } = req.body;
+
+    // Check if the 'update' field exists and is an integer
+    if (typeof update === 'undefined' || !Number.isInteger(update)) {
+      return res.status(400).json({ error: 'Invalid input: Update field must be set and contain an integer.' });
+    }
+  
   const query = new objectId(req.params.id);
   const updateField = req.body.updateField;
   const result = await db_client.getDb().db(dbName).collection(colName).updateOne(
@@ -105,10 +127,29 @@ const updateSize = async(req,res) =>
     } else {
       res.status(500).json(result.error || 'Some error occurred while updating the contact.');
     }
+}catch(error){
+  console.error('Error updating renown points:', error);
+    res.status(500).send('An error occurred while updating Army Size.');
+}
 };
 
 const updateLogo = async(req,res) =>
 {
+  try{
+    const { id } = req.params;
+    const { logoUrl } = req.body;
+
+    // Check if the 'logoUrl' field exists and is a non-empty string
+    if (!logoUrl || typeof logoUrl !== 'string' || logoUrl.trim() === '') {
+      return res.status(400).json({ error: 'Invalid input: Logo URL must be provided as a non-empty string.' });
+    }
+
+    // Check if the 'logoUrl' ends with a supported image extension
+    const supportedExtensions = ['.jpg', '.gif', '.bmp', '.png'];
+    const isValidExtension = supportedExtensions.some(ext => logoUrl.toLowerCase().endsWith(ext));
+    if (!isValidExtension) {
+      return res.status(400).json({ error: 'Invalid input: Logo URL must end with a supported image extension (jpg, gif, bmp, png).' });
+    }
   const query = new objectId(req.params.id);
   const updateField = req.body.updateField;
   const result = await db_client.getDb().db(dbName).collection(colName).updateOne(
@@ -119,9 +160,13 @@ const updateLogo = async(req,res) =>
     if (result.modifiedCount > 0) {
       res.status(204).send();
     } else {
-      res.status(500).json(result.error || 'Some error occurred while updating the contact.');
+      res.status(500).json(result.error || 'Some error occurred while updating your armies Logo.');
     }
-};
+  }catch (error) {
+      console.error('Error updating logo URL:', error);
+      res.status(500).send('An error occurred while updating the logo URL.');
+    }
+  };
 // Delete Statement
 const remove = async (req,res) =>
 {
