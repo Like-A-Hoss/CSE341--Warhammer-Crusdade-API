ebe8666b785799ec63cf2350d45c5b959ce9c4c1
Like-A-Hoss <hapleswithdice@gmail.com>
2023-06-10 21:20:43 -0500

    Added authentication to armies.

--- routes/armies.js
+++ routes/armies.js
@@ -2,14 +2,18 @@ const express = require('express');
 const router = express.Router();
 const path = require('../controllers/armies');
 const { validateId, validate } = require('../middleware/id_validate');
+const { authenticateOAuth } = require('../middleware/oauth');
 
+//Public Routes
 router.get('/', path.getAll);
 router.get('/:id', validateId, validate, path.getSearch);
-router.post('/', path.postNew);
-//router.put('/:id', path);
-router.put('/rp/:id', validateId, validate, path.updateRp);
-router.put('/size/:id', validateId, validate, path.updateSize);
-router.put('/logo/:id', validateId, validate, path.updateLogo);
-router.delete('/:id', validateId, validate, path.remove);
+
+
+// Protected Routes
+router.post('/', authenticateOAuth, path.postNew);
+router.put('/rp/:id', authenticateOAuth, validateId, validate, path.updateRp);
+router.put('/size/:id', authenticateOAuth, validateId, validate, path.updateSize);
+router.put('/logo/:id', authenticateOAuth, validateId, validate, path.updateLogo);
+router.delete('/:id', authenticateOAuth, validateId, validate, path.remove);
 
 module.exports = router;
